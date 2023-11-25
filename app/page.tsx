032353a10865230b8b15cd763899d1a3c96ad73f
Dmitry O <theicesilver121@mail.ru>
2023-11-25 21:15:01 +0300

    updateed: locomotive

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ import { useEffect } from 'react';
 import Intro from '@/components/main/intro';
 import Outro from '@/components/main/outro';
 import TechStack from '@/components/main/tech-stack';
-import Projects from '@/components/main/projects';
-import ProjeectItem from '@/ui/projects/item';
-import { projects } from '@/components/main/projects/data';
-import Link from 'next/link';
 
 export default function Home() {
     useEffect(() => {
