a17099dae0e19849b3c02566987389f932c0dc4e
Dmitry O <theicesilver121@mail.ru>
2023-11-29 18:03:20 +0300

    testing

--- components/main/masthead/index.tsx
+++ components/main/masthead/index.tsx
@@ -11,14 +11,7 @@ const Masthead: React.FC = () => {
         <div className={styles.masthead}>
             <div className={styles.masthead__container}>
                 <Title />
-                <div className={`hide-on-mobile ${styles.svg}`}>
-                    <Image
-                        src='/masthead/rainbow.svg'
-                        width={0}
-                        height={0}
-                        alt='svg'
-                    />
-                </div>
+            
                 <MobileButton />
             </div>
         </div>
